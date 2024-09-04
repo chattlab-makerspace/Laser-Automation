1f5dcb453aa367458603b35d62fc3c0a6ed66de7
TIIIM <ttsgeb@gmail.com>
2024-09-03 22:18:19 -0400

    timer initial and reset handling

--- LaserScript/LASimple/LASimple.ino
+++ LaserScript/LASimple/LASimple.ino
@@ -4,8 +4,8 @@
 #define OFF 1
 
 //outputs
-#define COOLER_RELAY 9
-#define FAN_RELAY 10
+#define CHILLER_RELAY 9
+#define EXHAUST_RELAY 10
 #define AIR_RELAY 11
 #define AUX_RELAY 12
 
@@ -14,6 +14,9 @@
 #define AIR_REQUEST 5
 #define JOB_RUNNING 6
 #define MANUAL_RESET 7
+
+#define CHILLER_REQUEST MACHINE_POWER
+#define EXHAUST_REQUEST JOB_RUNNING
 
 auto air = timer_create_default();
 auto exhaust = timer_create_default();
@@ -28,36 +31,42 @@ unsigned long exhaustDelay = 20 * second;
 unsigned long chillerDelay = 15 * minute;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(MACHINE_POWER, INPUT_PULLUP);
   pinMode(AIR_REQUEST, INPUT_PULLUP);
   pinMode(JOB_RUNNING, INPUT_PULLUP);
   pinMode(MANUAL_RESET, INPUT_PULLUP);
   
-  pinMode(COOLER_RELAY, OUTPUT);
-  pinMode(FAN_RELAY, OUTPUT);
+  pinMode(CHILLER_RELAY, OUTPUT);
+  pinMode(EXHAUST_RELAY, OUTPUT);
   pinMode(AIR_RELAY, OUTPUT);
   pinMode(AUX_RELAY, OUTPUT);
   
-  digitalWrite(COOLER_RELAY, OFF);
-  digitalWrite(FAN_RELAY, OFF);
+  digitalWrite(CHILLER_RELAY, OFF);
+  digitalWrite(EXHAUST_RELAY, OFF);
   digitalWrite(AIR_RELAY, OFF);
   digitalWrite(AUX_RELAY, OFF);
 }
 
 
 bool exhaustOff(void *){
-  digitalWrite(FAN_RELAY, OFF);
+  digitalWrite(EXHAUST_RELAY, OFF);
   return false;
 }
 
 bool chillerOff(void *){
-  digitalWrite(COOLER_RELAY, OFF);
+  digitalWrite(CHILLER_RELAY, OFF);
   return false;
 }
 
 bool airOff(void *){
   digitalWrite(AIR_RELAY, OFF);
   return false;
+}
+
+void timerClear(Timer<> timerToClear){
+    // ideally would only happen when timer is set, but library does not easily allow
+    timerToClear.cancel();
 }
 
 void loop() {
@@ -67,22 +76,36 @@ void loop() {
   // Air Handling
   if (digitalRead(MACHINE_POWER) == ON && digitalRead(AIR_REQUEST) == ON && digitalRead(AIR_RELAY) == OFF){
     digitalWrite(AIR_RELAY, ON);
+    timerClear(air);
   }
   if ((digitalRead(MACHINE_POWER) == OFF || (digitalRead(AIR_REQUEST) == OFF) && digitalRead(AIR_RELAY) == ON)){
     air.in(airDelay, airOff);
   }
   // Chiller Handling
-  if (digitalRead(MACHINE_POWER) == ON && digitalRead(COOLER_RELAY) == OFF){
-    digitalWrite(COOLER_RELAY, ON);
+  if (digitalRead(MACHINE_POWER) == ON && digitalRead(CHILLER_RELAY) == OFF){
+    digitalWrite(CHILLER_RELAY, ON);
+    timerClear(chiller);
   }
-  if (digitalRead(MACHINE_POWER) == OFF && digitalRead(COOLER_RELAY) == ON){
+  if (digitalRead(MACHINE_POWER) == OFF && digitalRead(CHILLER_RELAY) == ON){
     chiller.in(chillerDelay, chillerOff);
   }
   // Exhaust Handling
-  if (digitalRead(MACHINE_POWER) == ON && digitalRead(JOB_RUNNING) == ON && digitalRead(FAN_RELAY) == OFF){
-    digitalWrite(FAN_RELAY, ON);
+  if (digitalRead(MACHINE_POWER) == ON && digitalRead(EXHAUST_REQUEST) == ON){
+    Serial.println("Relay " + String(digitalRead(EXHAUST_RELAY)));
+    if (digitalRead(EXHAUST_RELAY) == OFF){
+      digitalWrite(EXHAUST_RELAY, ON);
+    } else {
+      //timerClear(exhaust);
+      Serial.println(exhaust.size());
+      Serial.println(exhaust.empty());
+      if(!exhaust.empty()){
+        exhaust.cancel();
+      }
+    }
   }
-  if ((digitalRead(MACHINE_POWER) == OFF || (digitalRead(JOB_RUNNING) == OFF) && digitalRead(FAN_RELAY) == ON)){
-    exhaust.in(exhaustDelay, exhaustOff);
+  if ((digitalRead(MACHINE_POWER) == OFF || (digitalRead(EXHAUST_REQUEST) == OFF) && digitalRead(EXHAUST_RELAY) == ON)){
+    if (exhaust.empty()){
+      exhaust.in(exhaustDelay, exhaustOff);
+    }
   }
 }
